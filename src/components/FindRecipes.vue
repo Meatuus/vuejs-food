e16b907495afb26ca2eff444c12b7c4a4bb8c350
Tyler <tcameronsmith@gmail.com>
2017-11-03 09:15:46 +1100

    initial commit, install vue, add component

---
+++ src/components/FindRecipes.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1>Recipe Finder</h1>
+        <form action="">
+            <input v-model="newIngredient" type="text">
+        </form>
+        <ul>
+            <li v-for="(item, index) in ingredientList" :key="index">{{ item }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FindRecipes",
+        data () {
+            return {
+                ingredientList: [
+                    "basil",
+                    "tomato",
+                    "onion",
+                    "salt",
+                    "pepper",
+                    "olive oil"
+                ],
+                newIngredient: ""
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    /* h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    } */
+</style>
